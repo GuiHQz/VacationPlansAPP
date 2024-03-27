7783d1d54404b57d8c50585dbfd6caecd718d8d3
GuiHQz <guilher.queiroz@gmail.com>
2024-03-27 14:40:18 -0300

    feat: New route added and imports changed

--- src/components/routes/router.tsx
+++ src/components/routes/router.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { HolidayPlans } from "../../pages/HolidayPlans/HolidayPlans";
-import { AddHolidayPlans } from "../../pages/AddHolidayPlans/AddHolidayPlans";
+import HolidayPlans from "../../pages/HolidayPlans/HolidayPlans";
+import AddHolidayPlans from "../../pages/AddHolidayPlans/AddHolidayPlans";
+import UpdateHolidayPlans from "../../pages/UpdateHolidayPlans/UpdateHolidayPlans";
 
 function Navigation() {
   return (
@@ -9,6 +10,7 @@ function Navigation() {
       <Routes>
         <Route path="/" element={<HolidayPlans />} />
         <Route path="/add" element={<AddHolidayPlans />} />
+        <Route path="/edit/:id" element={<UpdateHolidayPlans />} />
       </Routes>
     </BrowserRouter>
   );
